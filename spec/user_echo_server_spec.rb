417cd19f2957c5ae7e0ca031a1f9665129c07da8
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-01-02 22:34:58 +0000

    Test for welcome message now passes

--- spec/user_echo_server_spec.rb
+++ spec/user_echo_server_spec.rb
@@ -4,15 +4,30 @@ require 'spec_helper'
 
 describe UserEchoServer do
 
-  let(:output) { CommandLineView.new }
-  let(:userechoserver) { UserEchoServer.new(output) }
+  # let(:output) { CommandLineView.new }
+  # let(:userechoserver) { UserEchoServer.new(output) }
 
   it 'should be an instance of userechoserver' do
+    userechoserver = UserEchoServer.new
     userechoserver.should be_an_instance_of UserEchoServer
   end
 
   it 'should be an instance of commandlineview' do
+    output = CommandLineView.new
     output.should be_an_instance_of CommandLineView
   end
 
+  it 'prints out the welcome message' do
+    output = StringIO.new
+    view = CommandLineView.new(output)
+    message = view.welcome_message
+    expect(output.string).to eq("Welcome\n")
+  end
+
+  # it 'prints out the instruction message' do
+  #   output = StringIO.new
+  #   view = CommandLineView.new(output)
+  #   message = view.instruction_message
+  #   expect(output.string).to eq("Enter anything or exit to finish:\n")
+  # end
 end
