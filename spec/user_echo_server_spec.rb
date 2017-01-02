2577b5bf18ef00c813f559b43df4c58952979ef5
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-01-02 22:18:05 +0000

    Initialize test now pass

--- spec/user_echo_server_spec.rb
+++ spec/user_echo_server_spec.rb
@@ -1,5 +1,18 @@
-require_relative '../lib/user_echo_server.rb'
+require_relative '../lib/user_echo_server'
+require_relative '../lib/command_line_view'
+require 'spec_helper'
 
 describe UserEchoServer do
+
+  let(:output) { CommandLineView.new }
+  let(:userechoserver) { UserEchoServer.new(output) }
+
+  it 'should be an instance of userechoserver' do
+    userechoserver.should be_an_instance_of UserEchoServer
+  end
+
+  it 'should be an instance of commandlineview' do
+    output.should be_an_instance_of CommandLineView
+  end
 
 end
