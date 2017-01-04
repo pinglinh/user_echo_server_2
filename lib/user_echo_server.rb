1f50606a446ec2b2e7ec78c76a44b05344f229c1
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-01-04 19:38:35 +0000

    Added start method

--- lib/user_echo_server.rb
+++ lib/user_echo_server.rb
@@ -1,7 +1,8 @@
-require 'command_line_view'
-require 'user_echo_input'
+require_relative 'command_line_view'
+require_relative 'user_echo_input'
 
 class UserEchoServer
+EXIT = "exit"
 
   def initialize(input=STDIN, output=STDOUT)
     @input = UserEchoInput.new
@@ -9,6 +10,19 @@ class UserEchoServer
   end
 
   def start
+    @output.welcome_message
+    @output.instruction_message
+    @user_input = @input.read_line
+    until exit_entered? == true do
+      @output.echo_user_line(@user_input)
+      @user_input = @input.read_line
+    end
+    @output.goodbye_message
+  end
 
+  private
+
+  def exit_entered?
+    @user_input == EXIT
   end
 end
