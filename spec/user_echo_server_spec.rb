16aa2d53790ef9fefc73deb68d8aa86af27e50aa
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-01-04 20:09:28 +0000

    Removed command line view and user echo input tests

--- spec/user_echo_server_spec.rb
+++ spec/user_echo_server_spec.rb
@@ -3,48 +3,6 @@ require 'spec_helper'
 
 describe UserEchoServer do
 
-  it 'prints out the welcome message' do
-    output = StringIO.new
-    view = CommandLineView.new(output)
-    message = view.welcome_message
-    expect(output.string).to eq("Welcome!\n")
-  end
-
-  it 'prints out the instruction message' do
-    output = StringIO.new
-    view = CommandLineView.new(output)
-    message = view.instruction_message
-    expect(output.string).to eq("Enter anything or exit to finish:\n")
-  end
-
-  it 'prints out user\'s input with new line - 1 word' do
-    output = StringIO.new
-    view = CommandLineView.new(output)
-    user_view = view.echo_user_line("hello")
-    expect(output.string).to eq("hello\n")
-  end
-
-  it 'prints out user\'s input with new line - 2 words' do
-    output = StringIO.new
-    view = CommandLineView.new(output)
-    user_view = view.echo_user_line("hello linh")
-    expect(output.string).to eq("hello linh\n")
-  end
-
-  it 'prints out the goodbye message' do
-    output = StringIO.new
-    view = CommandLineView.new(output)
-    message = view.goodbye_message
-    expect(output.string).to eq("Goodbye!\n")
-  end
-
-  it 'takes user\'s input - 1 word' do
-    input = StringIO.new("hello\n")
-    user_echo_input = UserEchoInput.new(input)
-    line = user_echo_input.read_line
-    expect(line).to eq("hello")
-  end
-
   it 'echoes what the user wrote' do
     input = StringIO.new("one\ntwo\nthree\n")
     output = StringIO.new
