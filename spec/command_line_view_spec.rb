d4709e167be0333e5876e96dced9485dbc872e49
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-01-04 20:20:45 +0000

    Refactored command line view test

--- spec/command_line_view_spec.rb
+++ spec/command_line_view_spec.rb
@@ -1,36 +1,29 @@
 describe CommandLineView do
 
+let(:output) { StringIO.new }
+let(:view) { CommandLineView.new(output) }
+
 it 'prints out the welcome message' do
-    output = StringIO.new
-    view = CommandLineView.new(output)
     view.welcome_message
     expect(output.string).to eq("Welcome!\n")
   end
 
   it 'prints out the instruction message' do
-    output = StringIO.new
-    view = CommandLineView.new(output)
     view.instruction_message
     expect(output.string).to eq("Enter anything or exit to finish:\n")
   end
 
   it 'prints out user\'s input with new line - 1 word' do
-    output = StringIO.new
-    view = CommandLineView.new(output)
     view.echo_user_line("hello")
     expect(output.string).to eq("hello\n")
   end
 
   it 'prints out user\'s input with new line - 2 words' do
-    output = StringIO.new
-    view = CommandLineView.new(output)
     view.echo_user_line("hello linh")
     expect(output.string).to eq("hello linh\n")
   end
 
   it 'prints out the goodbye message' do
-    output = StringIO.new
-    view = CommandLineView.new(output)
     view.goodbye_message
     expect(output.string).to eq("Goodbye!\n")
   end
