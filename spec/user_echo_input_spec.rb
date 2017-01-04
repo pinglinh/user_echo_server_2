6255f8e9c379dce64463060b2f46e1c2a4937011
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-01-04 20:08:43 +0000

    Separated user echo input spec

---
+++ spec/user_echo_input_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../lib/user_echo_input'
+require 'spec_helper'
+
+describe UserEchoInput do
+
+  it 'takes user\'s input - 1 word' do
+    input = StringIO.new("hello\n")
+    user_echo_input = UserEchoInput.new(input)
+    line = user_echo_input.read_line
+    expect(line).to eq("hello")
+  end
+
+end
