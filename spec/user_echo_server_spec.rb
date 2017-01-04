6d1d55d749f2c41237bea85a7cfc1574212d36a8
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-01-04 20:24:33 +0000

    Added a test for when the server stops

--- spec/user_echo_server_spec.rb
+++ spec/user_echo_server_spec.rb
@@ -14,4 +14,17 @@ describe UserEchoServer do
       "Goodbye!\n"
       )
   end
+
+  it 'the server stops when user types exit' do
+    input = StringIO.new("one\nexit\n")
+    output = StringIO.new
+    server = UserEchoServer.new(input, output)
+    server.start
+    expect(output.string).to eq(
+      "Welcome!\n" +
+      "Enter anything or exit to finish:\n" +
+      "one\n" +
+      "Goodbye!\n"
+      )
+  end
 end
