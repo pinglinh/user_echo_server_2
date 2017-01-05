0420453fc4d7e84f6da6fb5a5e4bdd176f073ecf
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-01-05 18:55:59 +0000

    Corrected indentation

--- spec/user_echo_input_spec.rb
+++ spec/user_echo_input_spec.rb
@@ -1,10 +1,8 @@
 describe UserEchoInput do
-
   it 'takes user\'s input - 1 word' do
     input = StringIO.new("hello\n")
     user_echo_input = UserEchoInput.new(input)
     line = user_echo_input.read_line
     expect(line).to eq("hello")
   end
-
 end
