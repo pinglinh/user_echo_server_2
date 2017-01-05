b70c464b004e253bd6a1032e176fd3ffb38ef822
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-01-05 20:44:41 +0000

    Added two more tests to check for 1 and 2 word user inputs

--- spec/user_echo_server_spec.rb
+++ spec/user_echo_server_spec.rb
@@ -1,5 +1,32 @@
 describe UserEchoServer do
   it 'echoes what the user wrote' do
+    input = StringIO.new("one\n")
+    output = StringIO.new
+    server = UserEchoServer.new(input, output)
+    server.start
+    expect(output.string).to eq(
+      "Welcome!\n" +
+      "Enter anything or exit to finish:\n" +
+      "one\n" +
+      "Goodbye!\n"
+      )
+  end
+
+  it 'echoes what the user wrote' do
+    input = StringIO.new("one\ntwo\n")
+    output = StringIO.new
+    server = UserEchoServer.new(input, output)
+    server.start
+    expect(output.string).to eq(
+      "Welcome!\n" +
+      "Enter anything or exit to finish:\n" +
+      "one\n" +
+      "two\n" +
+      "Goodbye!\n"
+      )
+  end
+
+  it 'echoes what the user wrote' do
     input = StringIO.new("one\ntwo\nthree\n")
     output = StringIO.new
     server = UserEchoServer.new(input, output)
