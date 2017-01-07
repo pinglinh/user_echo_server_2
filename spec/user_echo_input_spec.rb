09159d4ff53abf4a9a739d4679d4965064b4e4bc
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-01-07 01:08:58 +0000

    Added test for 0 words

--- spec/user_echo_input_spec.rb
+++ spec/user_echo_input_spec.rb
@@ -1,4 +1,11 @@
 describe UserEchoInput do
+  it 'takes user\'s input - 0 word' do
+    input = StringIO.new("\n")
+    user_echo_input = UserEchoInput.new(input)
+    line = user_echo_input.read_line
+    expect(line).to eq("")
+  end
+
   it 'takes user\'s input - 1 word' do
     input = StringIO.new("hello\n")
     user_echo_input = UserEchoInput.new(input)
